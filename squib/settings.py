16affa3200e8656f9485fd7fac5246a3c033a37e
shyanukant rathi <rathishyanukant@gmail.com>
2023-07-29 16:07:00 +0530

    fix fetch issue

--- squib/settings.py
+++ squib/settings.py
@@ -107,7 +107,12 @@ WSGI_APPLICATION = "squib.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    "default": dj_database_url.config(default=os.environ.get('DATABASE_URL')),
+
+    'test': {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
     # {
     #     "ENGINE": "django.db.backends.sqlite3",
     #     "NAME": BASE_DIR / "db.sqlite3",
