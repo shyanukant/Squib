2932f08cc6356ce6efae54a4e62f7903b7b650d5
Shyanukant Rathi <72182251+shyanukant@users.noreply.github.com>
2023-10-06 09:54:35 +0000

    update connection string

--- squib/settings.py
+++ squib/settings.py
@@ -106,10 +106,10 @@ WSGI_APPLICATION = "squib.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 if DEBUG: 
     DATABASES = {
-        # "default": dj_database_url.config(default=os.environ.get('DATABASE_URL')),
-        {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            # "default": dj_database_url.config(default=os.environ.get('DATABASE_URL')),
+        "default" : {
+                "ENGINE": "django.db.backends.sqlite3",
+                "NAME": BASE_DIR / "db.sqlite3",
         }
     }
 else :
--- squib/urls.py
+++ squib/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from django.contrib import admin
 from django.urls import path, re_path, include
 from tweet.views import (
@@ -47,3 +48,9 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += [
+    re_path(r'^static/(?P<path>.*)$', serve,
+        {
+            'document_root': settings.STATIC_ROOT,
+        })
+]
